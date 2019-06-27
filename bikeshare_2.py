0fe24aa9df5f239fea60443417636922b87861ad
Anna Pedroni <anna.pedroni80@gmail.com>
2019-06-27 20:37:10 +0100

    docs: Clean up docstrings

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -20,7 +20,7 @@ def convert_month(arg):
         return months[arg - 1]
     else:
         return 'not a valid argument'
-        
+
 
 def get_filters():
     """
@@ -34,7 +34,7 @@ def get_filters():
     print('\nHello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
-        city = input('Which city would you like to explore: Chicago (c), New York City (ny) or Washington (w)?\nCity: ')       
+        city = input('Which city would you like to explore: Chicago (c), New York City (ny) or Washington (w)?\nCity: ')
         if city.lower() in ('chicago', 'c', 'cicago'):
             city = 'chicago'
             break
@@ -46,7 +46,7 @@ def get_filters():
             break
         else:
             print('\nSorry, you didn\'t enter a valid option.\n')
-            
+
 
 
     # get user input for month (all, january, february, ... , june)
@@ -65,9 +65,9 @@ def get_filters():
             break
         else:
             print('\nSorry, not a valid input.\nPlease, type in the name of the day you are interested in, or "all" for the whole week.')
-            
+
     print('\nYou chose to see the data for {}, month: {}, day of the week: {}\n'.format(city.upper(), month.upper(), day.upper()))
-    
+
     print('-'*40)
     return city, month, day
 
@@ -85,11 +85,11 @@ def load_data(city, month, day):
         df - Pandas DataFrame containing city data filtered by month and day
     """
     # load data file into a dataframe
-    df = pd.DataFrame(pd.read_csv(CITY_DATA[city]))   
-    
+    df = pd.DataFrame(pd.read_csv(CITY_DATA[city]))
+
     # store number of rows of the unfiltered df
     tot_rows = df.shape[0]
-    
+
     # convert the Start Time column to datetime
     df['Start Time'] = pd.to_datetime(df['Start Time'])
 
@@ -100,7 +100,7 @@ def load_data(city, month, day):
      # filter by month if applicable
     if month != 'all':
         chosen_month = convert_month(month)
-    
+
         # filter by month to create the new dataframe
         df = df[df['month']==chosen_month]
 
@@ -108,24 +108,24 @@ def load_data(city, month, day):
     if day != 'all':
         # filter by day of week to create the new dataframe
         df = df[df['day_of_week']==day.title()]
-    
+
     #number of rows after applying filters
     filtered_rows = df.shape[0]
-    
+
     message1 = '\nThe {} database has records for {} trips, from January to the end of June 2017.'
     message2 = 'Applying your filter (month: {}, day of week: {}) you get {} records.\n'
-    
+
     # gives info about the number of records used for the following statistics
     print(message1.format(city.upper(), tot_rows))
     if filtered_rows != tot_rows:
         print(message2.format(month.upper(), day.upper(), filtered_rows))
-    
-    print('-'*40)    
+
+    print('-'*40)
 
     return df
 
 
-#!!! ATTENZIONE: sistemare messaggio output e info in caso di filtro mese/giorno. input utente?
+
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
@@ -140,7 +140,7 @@ def time_stats(df):
     else:
         m_c_month = convert_month(df['month'].mode()[0])
         print('The most popular MONTH is: {}'.format(m_c_month.upper()))
-    
+
 
     # display the most common day of week
     d = df['day_of_week'].unique()
@@ -158,21 +158,21 @@ def time_stats(df):
     # find the most common hour (from 0 to 23)
     m_c_hour = df['Hour'].mode()[0]
     print('The most popular HOUR is: {}'.format(m_c_hour))
-   
-    
-    
+
+
+
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
 
-    
+
 def station_stats(df):
     """Displays statistics on the most popular stations and trip."""
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
-    
-    
+
+
     # finds the most commonly used start station
     m_c_start = df['Start Station'].mode()[0]
 
@@ -195,13 +195,13 @@ def trip_duration_stats(df):
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
-    
+
     # convert the End Time column to datetime
     df['End Time'] = pd.to_datetime(df['End Time'])
 
     # find the travel time
     df['Travel Time'] = df['End Time'] - df['Start Time']
-    
+
     # display total travel time
     print('Our bicicles have been ridden a TOTAL of {} in the selected period.'.format(df['Travel Time'].sum()))
 
@@ -232,7 +232,7 @@ def user_stats(df):
     else:
         print('No gender information available for the users of this city.')
 
-    
+
     # Display earliest, most recent, and most common year of birth, if the info is available in the DB
     if 'Birth Year' in df.columns:
         earl = int(df['Birth Year'].min())
@@ -241,8 +241,8 @@ def user_stats(df):
         print('\nAbout the YEAR OF BIRTH,\n- the earliest is: {}\n- the most recent is: {}\n- the most common is: {}'.format(earl, most_rec, most_comm))
     else:
         print('No age information available for the users of this city.')
-    
-    
+
+
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -250,7 +250,7 @@ def user_stats(df):
 
 def raw_data(df, city, month, day):
     """asks user if they want to see raw data. displays them 5 at a time and asks if they want more"""
-    
+
     #takes away the columns added by the previous functions, but keeps the filtered DB
     #I use the df for friendlier display (lines of the .csv file should be formatted anyway) and to show the filtered records
     del df['Travel Time']
@@ -258,16 +258,16 @@ def raw_data(df, city, month, day):
     del df['month']
     del df['day_of_week']
     del df['Hour']
-    
-    # to have some context    
+
+    # to have some context
     df_shape = df.shape
     print('The database for the city of {}, month: {}, day of the week: {}, has {} rows.'.format(city.upper(), month.upper(), day.upper(), df_shape[0]))
-    
+
     #ask the user if they want to see raw data
     raw_1 = input('Would you like to display the raw data? (Type "yes" to see the raw data, anything else to dismiss): ')
-    
+
     if raw_1.lower() == 'yes':
-       
+
         print(raw_1)
         print(df[:5])
         stop = df_shape[0]
@@ -276,12 +276,12 @@ def raw_data(df, city, month, day):
             if more_raw.lower() != 'no':
                 print(df[i : i+5])
             else:
-                break      
+                break
     else:
         print('No raw data requested')
-            
+
     print('-'*40)
-    
+
 
 def main():
     while True:
@@ -292,9 +292,9 @@ def main():
         station_stats(df)
         user_stats(df)
         trip_duration_stats(df)
-        
+
         raw_data(df, city, month, day)
-        
+
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             print('Good bye!')
